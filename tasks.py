149733163f5e91ad7ffe0201b2333db7b416a591
ivannikovoleg <84902438+ivannikovoleg@users.noreply.github.com>
2021-11-27 16:34:12 +0300

    Commit

--- tasks.py
+++ tasks.py
@@ -6,42 +6,32 @@ from RPA.PDF import PDF
 from RPA.Excel.Files import Files
 
 
-def wait_for_downloads(path: str):
-    print('Waiting for downloads', end='')
-    while any([filename.endswith('.crdownload') for filename in
-               os.listdir(path)]):
-        time.sleep(2)
+def wait_for_downloads_file(filename: str):
+    print(f'Waiting for {filename}', end='')
+    while filename not in os.listdir('output'):
+        time.sleep(1)
         print(".", end="")
-    print('\nAll files downloaded!')
+    print('\nFile downloaded!')
 
 
-def rpa_parse_pdf_data(filename: str) -> list:
+def rpa_parse_pdf_data(filename: str) -> dict:
     pdf = PDF()
-    investment_name = 'Name of this Investment: '
-    uii = '2. Unique Investment Identifier (UII): '
+    start = 'Name of this Investment: '
+    sep = '2. Unique Investment Identifier (UII): '
     end = 'Section B:'
-    text = pdf.get_text_from_pdf(filename)
-    page_text = text[1]
-    investment_name_index = page_text.find(investment_name) + len(investment_name)
-    uii_index_start = page_text.find(uii)
-    uii_index_end = uii_index_start + len(uii)
+    page_text = pdf.get_text_from_pdf(filename, 1)[1]
+    investment_name_index = page_text.find(start) + len(start)
+    uii_index_start = page_text.find(sep)
+    uii_index_end = uii_index_start + len(sep)
     end_index = page_text.find(end)
-    return [page_text[investment_name_index: uii_index_start],
-            page_text[uii_index_end:end_index]]
-
-
-def check_entries_by_elements(path: str, table: list):
-    file_list = [os.path.join(path, file) for file in os.listdir(path) if file.endswith('.pdf')]
-    for file in file_list:
-        v = rpa_parse_pdf_data(file)
-        print(f'Checking entries {v[1]}: {v[0]}')
-        for element in table:
-            if v[0] == element or v[1] == element:
-                print(f'{element} in table.')
+    investment = page_text[investment_name_index: uii_index_start]
+    uii = page_text[uii_index_end:end_index]
+    return {'uii': uii, 'investment': investment}
 
 
 def get_departments(webdriver: Selenium):
     webdriver.open_headless_chrome_browser('https://itdashboard.gov/')
+    # webdriver.open_chrome_browser('https://itdashboard.gov/')
     webdriver.click_element("//a[@href='#home-dive-in']")
     webdriver.wait_until_element_is_visible('//div[@id="node-23"]//div[@id="agency-tiles-container"]')
     departments = webdriver.find_elements('//div[@id="agency-tiles-widget"]//span[@class="h4 w200"]')
@@ -70,6 +60,23 @@ def get_agencies_table(webdriver: Selenium, dep_to_scrap: str) -> list:
         "//a[@id='investments-table-object_last' and contains(@class, 'disabled')]",
         timedelta(seconds=30))
     print('Show all entries.')
+    table_rows = webdriver.find_elements('//div[@id="investments-table-object_wrapper"]//tbody//tr')
+    for element in table_rows:
+        urls = element.find_elements_by_xpath('.//a')
+        for url in urls:
+            link = url.get_attribute('href')
+            file_name = f"{link[link.rfind('/') + 1:]}.pdf"
+            download_file_single(webdriver, link, file_name)
+            values = rpa_parse_pdf_data(f'output/{file_name}')
+            print(f"Check {values['uii']}: {values['investment']} in table.")
+            if (values['uii'] in element.text) and (values['investment'] in element.text):
+                print('Table contains both values.')
+            elif (values['uii'] in element.text) and (values['investment'] not in element.text):
+                print(f'Table contain only {values["uii"]}.')
+            elif (values['uii'] not in element.text) and (values['investment'] in element.text):
+                print(f'Table contain only {values["investment"]}.')
+            else:
+                print("Table doesn't contain values.")
     table = [i.text for i in webdriver.find_elements('//div[@id="investments-table-object_wrapper"]//tbody//tr//td')
              if i.text != '']
     return table
@@ -86,21 +93,21 @@ def write_agencies(workbook: Files, table: list):
                 col_num = 1
             workbook.set_cell_value(row_num, col_num, data)
             col_num += 1
+    print('Agencies: Done!')
 
 
-def download_file(webdriver: Selenium):
-    urls = webdriver.find_elements('//div[@id="investments-table-object_wrapper"]//tbody//tr//a')
-    links = [url.get_attribute("href") for url in urls]
-    for link in links:
-        webdriver.go_to(link)
-        webdriver.wait_until_element_is_visible(
-            '//div[@id="investment-quick-stats-container"]//div[@id="business-case-pdf"]', timedelta(seconds=30))
-        webdriver.click_element('//div[@id="investment-quick-stats-container"]//div[@id="business-case-pdf"]')
-        webdriver.wait_until_element_does_not_contain(
-            '//div[@id="investment-quick-stats-container"]//div[@id="business-case-pdf"]', 'Generating PDF...',
-            timedelta(seconds=30))
-        time.sleep(5)
-        print('Downloading file.')
+def download_file_single(webdriver: Selenium, link: str, filename: str):
+    webdriver.execute_javascript(f'window.open("{link}")')
+    webdriver.switch_window("NEW")
+    webdriver.wait_until_element_is_visible(
+        '//div[@id="investment-quick-stats-container"]//div[@id="business-case-pdf"]', timedelta(seconds=30))
+    webdriver.click_element('//div[@id="investment-quick-stats-container"]//div[@id="business-case-pdf"]')
+    webdriver.wait_until_element_does_not_contain(
+        '//div[@id="investment-quick-stats-container"]//div[@id="business-case-pdf"]', 'Generating PDF...',
+        timedelta(seconds=30))
+    wait_for_downloads_file(filename)
+    webdriver.close_window()
+    webdriver.switch_window("MAIN")
 
 
 def main():
@@ -119,10 +126,7 @@ def main():
     write_agencies(workbook, table)
     workbook.save_workbook('output/write_data.xlsx')
     workbook.close_workbook()
-    download_file(driver)
-    wait_for_downloads('output')
     driver.close_all_browsers()
-    check_entries_by_elements('output', table)
 
 
 if __name__ == '__main__':
